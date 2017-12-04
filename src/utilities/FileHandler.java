085b41cbffc3edde0cfbc08005c7fde61b841103
Gaurav Nitin Gandhi <gandhi.ga@husky.neu.edu>
2017-12-04 15:55:30 -0500

    Added file handler in utilities package

---
+++ src/utilities/FileHandler.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author Gaurav Gandhi
+ *
+ */
+public class FileHandler {
+	
+	//private static final String FILE_NAME = "Links.txt";
+	private BufferedWriter B_WRITER;
+	private FileWriter F_WRITER;
+	private BufferedReader B_READER;
+	private FileReader F_READER;
+	public FileHandler(String file_name, int type) throws IOException {
+		if(type == 0) {
+			F_WRITER = new FileWriter(file_name);
+			B_WRITER = new BufferedWriter(F_WRITER);
+		}
+		else {
+			F_READER = new FileReader(file_name);
+			B_READER = new BufferedReader(F_READER);
+		}
+	}
+	
+	public void emptyFile() throws IOException {
+		B_WRITER.write("");
+	}
+	
+	public void addText(String str) throws IOException {
+		B_WRITER.append(str);
+	}
+	
+	public String readLine() throws IOException {
+		return B_READER.readLine();
+	}
+	
+	public void closeConnection() throws IOException {
+		B_WRITER.close();
+		F_WRITER.close();
+	}
+	
+	
+	
+}
+
