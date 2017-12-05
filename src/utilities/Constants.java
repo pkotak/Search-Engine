18bc8399c0f6d87564bcab5ac8d7a4f4b2e45c8e
Paarth Kotak <kotak.p@husky.neu.edu>
2017-12-04 20:16:34 -0500

    Constants file initial commit.

---
+++ src/utilities/Constants.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class Constants {
+    public static final String raw_corpus_dir = "Documents\\cacm";
+    public static final String lucene_output_dir ="Results\\LuceneOutput\\";
+    public static final String query_dir ="ProblemStatement\\cacm.query.txt";
+    public static final String lucene_index_path ="Documents\\LuceneIndex";
+}
