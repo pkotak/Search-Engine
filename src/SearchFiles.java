1e9e54075aa3aec856bf1338ba2d39225e88d49f
Paarth Kotak <kotak.p@husky.neu.edu>
2017-12-08 20:15:34 -0500

    code cleanup

--- src/SearchFiles.java
+++ src/SearchFiles.java
@@ -27,61 +27,28 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 import utilities.Constants;
-import utilities.FileHandler;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-/** Simple command-line based search demo. */
 public class SearchFiles {
 
-
-    /** Simple command-line based search demo. */
     public static List<Result> searchQueries(String query, int query_id) throws Exception {
-    	
     	query = query.replaceAll("(?=[]\\[+&|!(){}^\"~*?:\\\\-])", "\\\\").replaceAll("/", "\\\\/");
-        SearchFiles sf = new SearchFiles();
         String index = Constants.LUCENE_INDEX_DIR;
         String field = "contents";
-//        String file_content = sf.generateFileContent();
-//        int query_id = 1;
-//        List<String> processed_query = sf.getProcessedQueryList(file_content);
         IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
         IndexSearcher searcher = new IndexSearcher(reader);
         Analyzer analyzer = new SimpleAnalyzer();
         QueryParser parser = new QueryParser(field, analyzer);
 
-//        for(String q: processed_query){
-            Query q = parser.parse(query);
-            System.out.println("Searching for: " + q.toString(field));
-            return doPagingSearch(searcher, q, query_id);
-//            query_id++;
-//        }
+        Query q = parser.parse(query);
+        System.out.println("Searching for: " + q.toString(field));
+        return doPagingSearch(searcher, q, query_id);
     }
 
-     String generateFileContent() throws IOException {
-        FileHandler file_reader = new FileHandler(Constants.QUERY_FILE,1);
-        StringBuilder content = new StringBuilder();
-        String currentLine;
-        while((currentLine = file_reader.readLine()) != null) {
-            content.append(currentLine);
-        }
-        return content.toString();
-    }
-     List<String> getProcessedQueryList(String file_content){
-        List<String> query_list = new ArrayList<>();
-        String[] splitByDoc = file_content.split("</DOC>");
-
-        for (String s : splitByDoc) {
-//            int queryId = Integer.parseInt(s.split("</DOCNO>")[0].replace("<DOC>","")
-//                    .replace("<DOCNO>", "").trim());
-            String query = s.split("</DOCNO>")[1].replace("</DOC>", "");
-            query_list.add(query.replaceAll("(?=[]\\[+&|!(){}^\"~*?:\\\\-])", "\\\\").replaceAll("/", "\\\\/"));
-        }
-        return query_list;
-    }
     /**
      * This demonstrates a typical paging search scenario, where the search engine presents
      * pages of size n to the user. The user can then go to the next page if interested in
@@ -93,24 +60,16 @@ public class SearchFiles {
      *
      */
     public static List<Result> doPagingSearch(IndexSearcher searcher, Query query, int query_id) throws IOException {
-
         TopDocs results = searcher.search(query, 100);
         ScoreDoc[] hits = results.scoreDocs;
-//        String sys_name = "Lucene";
-//        String content = "";
         List<Result> resultList = new ArrayList<>();
         for(int i = 0; i < hits.length; i++ ){
             Document doc = searcher.doc(hits[i].doc);
             String doc_id = doc.get("path").substring(doc.get("path").lastIndexOf("\\")+1,doc.get("path").length());
             if (doc_id != null) {
                 resultList.add(new Result1(doc_id,hits[i].score, query_id, "Lucene", "Parsed_punctuated"));
-//                content += query_id+" "+"Q0 "+doc_id+" "+(i+1)+" "+hits[i].score+" "+sys_name+"\n";
             }
         }
-//        FileHandler file_writer = new FileHandler(Constants.LUCENE_OUTPUT_DIR+"q"+query_id+".txt",0);
-//        file_writer.addText(content);
-//
-//        file_writer.closeConnection();
         return resultList;
     }
 }
